9a6d8becae373946e828d27396fb7ba89907cfa3
evwilkin <evan.wilkinson@gmail.com>
2016-04-13 15:09:02 -0700

    fixed navbar fade on scroll

--- js/script.js
+++ js/script.js
@@ -1,39 +1,44 @@
 $(document).ready(function(){
+
   // Collapse navbar
   $(".button-collapse").sideNav();
 
-  // Run fullpage.js
-  $('#fullpage').fullpage({
-    menu: ".nav",
-    autoScrolling: false,
-    resize: true
-  });
+  // Smooth Scroll
+  $('a').smoothScroll();
 
   // Fade in home page
-  $("#developer").fadeIn(1000).delay(1000).removeClass("hide");
-  $("#designer").fadeIn(1500).delay(1200).removeClass("hide");
-  $("#doer").fadeIn(2000).delay(1500).removeClass("hide");
+  setTimeout(function() {
+    $("#developer").fadeIn(1200).removeClass("hide");
+  }, 800);
+  setTimeout(function() {
+    $("#designer").fadeIn(1200).removeClass("hide");
+  }, 1600);
+  setTimeout(function() {
+    $("#doer").fadeIn(1200).removeClass("hide");
+  }, 2400);
 
   // Add home button to navbar when home page not displayed
   var headerHeight = $("header").height();
   $(window).scroll(function() {
     var wScroll = $(window).scrollTop();
-    if (wScroll >= headerHeight - 100) {
+    if (wScroll >= headerHeight * .85) {
       $("a.brand-logo").fadeIn(350).removeClass("hide");
+      $("nav").fadeIn(200).addClass("background");
     } else {
-      $("a.brand-logo").fadeOut(350).addClass("hide");
+      $("a.brand-logo").fadeOut(350);
+      $("nav").removeClass("background");
     }
   });
 });
 
-// var home = document.getElementById("home");
-// var dimensions = home.getClientRects()[0];
-// var pattern = Trianglify({
-//     width: dimensions.innerWidth,
-//     height: dimensions.innerHeight,
-//     cell_size: 40, 
-//     seed: 'ywn3w', 
-//     x_colors: 'random'
-// });
+var home = document.getElementById("home");
+var dimensions = home.getClientRects()[0];
+var pattern = Trianglify({
+    width: dimensions.width,
+    height: dimensions.height,
+    cell_size: 40, 
+    seed: 'ywn3w', 
+    x_colors: 'random'
+});
 
-// home.appendChild(pattern.canvas());
+home.appendChild(pattern.canvas());
