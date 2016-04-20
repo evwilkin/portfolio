cf9455c4d25ea7ba88c20ec956c13e34dd5bce6d
evwilkin <evan.wilkinson@gmail.com>
2016-04-19 19:56:44 -0700

    fixed scrolling, changed background to svg, adjusted navbar fadeout

--- js/script.js
+++ js/script.js
@@ -21,8 +21,8 @@ $(document).ready(function(){
   var headerHeight = $("header").height();
   $(window).scroll(function() {
     var wScroll = $(window).scrollTop();
-    if (wScroll >= headerHeight * .85) {
-      $("a.brand-logo").fadeIn(350).removeClass("hide");
+    if (wScroll >= headerHeight) {
+      $("a.brand-logo").fadeIn(500).removeClass("hide");
       $("nav").fadeIn(200).addClass("background");
     } else {
       $("a.brand-logo").fadeOut(350);
@@ -30,15 +30,3 @@ $(document).ready(function(){
     }
   });
 });
-
-var home = document.getElementById("home");
-var dimensions = home.getClientRects()[0];
-var pattern = Trianglify({
-    width: dimensions.width,
-    height: dimensions.height,
-    cell_size: 40, 
-    seed: 'ywn3w', 
-    x_colors: 'random'
-});
-
-home.appendChild(pattern.canvas());
